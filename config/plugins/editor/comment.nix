77186caecafc5428852e4c8d6c8433c9285ac7ca
jasmine <its.jassy@pm.me>
2024-08-06 22:53:30 +0800

    Update comment keybinds

--- config/plugins/editor/comment.nix
+++ config/plugins/editor/comment.nix
@@ -3,6 +3,31 @@
 {
   plugins.comment = {
     enable = true;
+    settings = {
+      # LHS of toggle mappings in NORMAL mode
+      toggler = {
+        # Line-comment toggle keymap
+        line = "<leader>c<space>";
+        # Block-comment toggle keymap
+        block = "<leader>c<enter>";
+      };
+      # LHS of operator-pending mappings in NORMAL and VISUAL mode
+      opleader = {
+        # Line-comment keymap
+        line = "<leader>c<space>";
+        # Block-comment keymap
+        block = "<leader>c<enter>";
+      };
+      # LHS of extra mappings
+      extra = {
+        # Add comment on the line above
+        above = "<leader>co";
+        # Add comment on the line below
+        below = "<leader>ce";
+        # Add comment at the end of line
+        eol = "<leader>ca";
+      };
+    };
   };
 }
 
