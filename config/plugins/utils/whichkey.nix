e3dc3f07d69bcb0c9df5875aefd7dbf3877be794
jasmine <its.jassy@pm.me>
2024-08-07 14:29:15 +0800

    Update whichkey

--- config/plugins/utils/whichkey.nix
+++ config/plugins/utils/whichkey.nix
@@ -4,5 +4,80 @@
   plugins.which-key = {
     enable = true;
   };
+
+  # Prefixes, No operation.
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>f";
+      action = "<Nop>";
+      options = {
+        desc = "+find/files";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>s";
+      action = "<Nop>";
+      options = {
+        desc = "+search";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>h";
+      action = "<Nop>";
+      options = {
+        desc = "+hunks";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>t";
+      action = "<Nop>";
+      options = {
+        desc = "+toggle";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>b";
+      action = "<Nop>";
+      options = {
+        desc = "+buffer";
+      };
+    }
+
+    {
+      mode = [ "n" "v" ];
+      key = "<leader>c";
+      action = "<Nop>";
+      options = {
+        desc = "+comment";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>g";
+      action = "<Nop>";
+      options = {
+        desc = "+git";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>l";
+      action = "<Nop>";
+      options = {
+        desc = "+list";
+      };
+    }
+  ];
 }
 
