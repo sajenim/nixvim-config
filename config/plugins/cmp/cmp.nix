6173719455fc65324cdcb6411564c4f36a6adc2f
jasmine <its.jassy@pm.me>
2024-08-12 21:01:30 +0800

    feat: cmp-path + keybinds

--- config/plugins/cmp/cmp.nix
+++ config/plugins/cmp/cmp.nix
@@ -8,6 +8,7 @@
       sources = [
         {name = "nvim_lsp";} # LSP source for cmp
         {name = "luasnip";} # Snippets source for cmp
+        {name = "path";} # Filesystem paths
       ];
       # The snippet expansion function.
       snippet.expand = ''
@@ -18,15 +19,15 @@
       # cmp mappings declaration
       mapping = {
         # Select next/previous item.
-        "<C-e>" = "cmp.mapping.select_next_item()";
-        "<C-o>" = "cmp.mapping.select_prev_item()";
+        "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         # Scroll through documentation.
         "<C-s>" = "cmp.mapping.scroll_docs(-4)";
         "<C-t>" = "cmp.mapping.scroll_docs(4)";
         # Confirm selection.
-        "<C-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
         # Exit completion.
-        "<C-Esc>" = "cmp.mapping.abort()";
+        "<C-e>" = "cmp.mapping.abort()";
       };
     };
     # Scans the sources array and enable the corresponding plugins if they are known to nixvim.
