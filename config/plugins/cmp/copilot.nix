feb76796379feb34bea75f90aff832aea0b7cb73
jasmine <its.jassy@pm.me>
2025-03-23 18:41:22 +0800

    update copilot plugin

--- config/plugins/cmp/copilot.nix
+++ config/plugins/cmp/copilot.nix
@@ -1,5 +1,14 @@
 {...}: {
   plugins.copilot-vim = {
     enable = true;
+    settings = {
+      filetypes = {
+        "*" = false;
+        "nix" = true;
+      };
+      workspace_folders = [
+        "/home/sajenim/.repositories/dotfiles.nix/"
+      ];
+    };
   };
 }
