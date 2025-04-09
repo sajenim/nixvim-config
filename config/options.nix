16b55e07a1e81255298fddd19cea28c6f26e972e
jasmine <its.jassy@pm.me>
2025-04-09 22:52:03 +0800

    chore: refactor

--- config/options.nix
+++ config/options.nix
@@ -1,17 +1,18 @@
 {...}: {
   config.opts = {
     # General
-    number = true; # Display line numbers.
-    relativenumber = true; # Enable relative line numbers
-    showmode = false; # Do not show the mode we're editing in.
-    swapfile = false; # Disable vim swapfile.
-    clipboard = "unnamedplus"; # Use the clipboard for all operations.
-    conceallevel = 1; # Show concealed characters (like markdown).
+    number = true;
+    relativenumber = true;
+    showmode = false;
+    swapfile = false;
+    clipboard = "unnamedplus";
+    conceallevel = 1;
+    updatetime=100;
 
     # Tabs & Spaces
-    tabstop = 2; # Set the width of a tab character.
-    softtabstop = 2; # Set the number of columns for a tab.
-    shiftwidth = 2; # Set the number of spaces for each step of (auto)indent.
-    expandtab = true; # Convert tabs to spaces.
+    tabstop = 2;
+    softtabstop = 2;
+    shiftwidth = 2;
+    expandtab = true;
   };
 }
