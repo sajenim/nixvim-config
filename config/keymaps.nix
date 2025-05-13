3fed17452d35812fd9a6a9bc3fd4c98d623d3f32
jasmine <its.jassy@pm.me>
2025-05-13 23:59:58 +0800

    feat: <control><direction> window navigation

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -11,6 +11,42 @@
       };
     }
 
+    { # focus window left
+      mode = ["n"];
+      key = "<C-Left>";
+      action = "<C-w>h";
+      options = {
+        silent = true;
+      };
+    }
+
+    { # focus window down
+      mode = ["n"];
+      key = "<C-Down>";
+      action = "<C-w>j";
+      options = {
+        silent = true;
+      };
+    }
+
+    { # focus window up
+      mode = ["n"];
+      key = "<C-Up>";
+      action = "<C-w>k";
+      options = {
+        silent = true;
+      };
+    }
+
+    { # focus window right
+      mode = ["n"];
+      key = "<C-Right>";
+      action = "<C-w>l";
+      options = {
+        silent = true;
+      };
+    }
+
     { # buffer prefix
       mode = "n";
       key = "<leader>b";
