4a6499f52c29b0ea40b2dc6ca50ce85d8186e56e
jasmine <its.jassy@pm.me>
2025-06-24 19:34:43 +0800

    update keybindings for fugitive and gitsigns

--- config/plugins/git/gitsigns.nix
+++ config/plugins/git/gitsigns.nix
@@ -15,6 +15,16 @@
     }
 
     {
+      mode = ["n"];
+      key = "<leader>gS";
+      action = "<cmd>Gitsigns stage_buffer<cr>";
+      options = {
+        silent = true;
+        desc = "Stage Buffer";
+      };
+    }
+
+    {
       mode = ["n" "v"];
       key = "<leader>gr";
       action = "<cmd>Gitsigns reset_hunk<cr>";
@@ -25,27 +35,7 @@
     }
 
     {
-      mode = "n";
-      key = "<leader>gS";
-      action = "<cmd>Gitsigns stage_buffer<cr>";
-      options = {
-        silent = true;
-        desc = "Stage Buffer";
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>gu";
-      action = "<cmd>Gitsigns undo_stage_hunk<cr>";
-      options = {
-        silent = true;
-        desc = "Undo Staged Hunk";
-      };
-    }
-
-    {
-      mode = "n";
+      mode = ["n"];
       key = "<leader>gR";
       action = "<cmd>Gitsigns reset_buffer<cr>";
       options = {
@@ -55,28 +45,8 @@
     }
 
     {
-      mode = "n";
-      key = "<leader>gp";
-      action = "<cmd>Gitsigns preview_hunk_inline<cr>";
-      options = {
-        silent = true;
-        desc = "Preview Hunk";
-      };
-    }
-
-    {
-      mode = "n";
+      mode = ["n"];
       key = "<leader>gb";
-      action = "<cmd>Gitsigns blame<cr>";
-      options = {
-        silent = true;
-        desc = "View Blame";
-      };
-    }
-
-    {
-      mode = "n";
-      key = "<leader>gtb";
       action = "<cmd>Gitsigns toggle_current_line_blame<cr>";
       options = {
         silent = true;
@@ -85,22 +55,32 @@
     }
 
     {
-      mode = "n";
-      key = "<leader>gd";
-      action = "<cmd>Gitsigns diffthis<cr>";
+      mode = ["n"];
+      key = "<leader>gB";
+      action = "<cmd>Gitsigns blame<cr>";
       options = {
         silent = true;
-        desc = "View Diff";
+        desc = "View Blame";
       };
     }
 
     {
-      mode = "n";
-      key = "<leader>gtd";
-      action = "<cmd>Gitsigns toggle_deleted<cr>";
+      mode = ["n"];
+      key = "[h";
+      action = "<cmd>Gitsigns prev_hunk<cr>";
       options = {
         silent = true;
-        desc = "Toggle Deleted";
+        desc = "Goto Previous Hunk";
+      };
+    }
+
+    {
+      mode = ["n"];
+      key = "]h";
+      action = "<cmd>Gitsigns next_hunk<cr>";
+      options = {
+        silent = true;
+        desc = "Goto Next Hunk";
       };
     }
   ];
