5d15d232763b4f2457b7d9b4015824861a133576
jasmine <its.jassy@pm.me>
2025-07-09 03:34:48 +0800

    vimtex set package

--- config/plugins/utils/vimtex.nix
+++ config/plugins/utils/vimtex.nix
@@ -1,6 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   plugins.vimtex = {
     enable = true;
+    texlivePackage = pkgs.texliveFull;
+
+    # Settings for the VimTeX plugin
     settings = {
       view_method = "zathura";
     };
